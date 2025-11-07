ef1b4a9095d67f0f03c952a40f35acc405564e2e
Keynes263 <79570052+Keynes263@users.noreply.github.com>
2025-11-07 21:20:16 +0200

    Update data_cleaning.py

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,10 +1,7 @@
 import pandas as pd
-
 # Load dataset
 df = pd.read_xlsx('../data/cape_fur_seal_entanglement_2021_2025.xlsx')
-
 # Display first few rows
 print(df.head())
-
 # Basic summary
 print(df.describe())
